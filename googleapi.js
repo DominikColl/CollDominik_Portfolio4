33ae7b261484b1c0d02fd874a3fcffe6bbf2a8fc
DominikColl <DAColl@student.fullsail.edu>
2019-07-28 21:24:11 -0400

    polishing

--- googleapi.js
+++ googleapi.js
@@ -1,7 +1,8 @@
-var map;
+var myMap;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  myMap = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.5155, lng: -122.6793},
     zoom: 18
   });
 }
+// Error becuase google doesnt have my billing information
